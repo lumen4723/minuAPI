db54e2cc1473c38fd4123502bc15110d01de41e2
minu <lumen@minuset.com>
2025-01-17 04:22:25 +0000

    inital setting

---
+++ src/main/java/com/lumen/minuAPI/MinuApiApplication.java
@@ -0,0 +1,13 @@
+package com.lumen.minuAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinuApiApplication.class, args);
+	}
+
+}

6d869da3946ba0377d58ee0f887cb500783ab2dd
minu <lumen@xminu.com>
2025-02-11 11:39:37 +0000

    OauthController에서 OAuth 인증 URL을 간소화하고 불필요한 코드를 제거하여 응답 형식을 개선함

--- src/main/java/com/lumen/minuAPI/Oauth/OauthController.java
+++ src/main/java/com/lumen/minuAPI/Oauth/OauthController.java
@@ -2,9 +2,7 @@ package com.lumen.minuAPI.Oauth;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
@@ -19,37 +17,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OauthController {
     @GetMapping("")
     public ResponseEntity<?> Oauth() {
-        List<Map<String, String>> oauthList = new ArrayList<>();
+        Map<String, String> result = new HashMap<>();
 
-        Map<String, String> googleAuth = new HashMap<>();
-        googleAuth.put("Google", "https://api.xminu.com/oauth2/authorization/google");
-        oauthList.add(googleAuth);
-        
+        result.put("Google", "https://api.xminu.com/oauth2/authorization/google");
+        result.put("Github", "https://api.xminu.com/oauth2/authorization/github");
+        result.put("Discord", "https://api.xminu.com/oauth2/authorization/discord");
+        result.put("Naver", "https://api.xminu.com/oauth2/authorization/naver");
+        result.put("Kakao", "https://api.xminu.com/oauth2/authorization/kakao");
+
+        // 라인은 profile만 제대로 가져오고 나머지는 안됨
+        result.put("Line", "https://api.xminu.com/oauth2/authorization/line");
+
         // facebook은 비지니스 인증을 받아야 로그인시 Warning이 뜨지않음
-        Map<String, String> facebookAuth = new HashMap<>();
-        facebookAuth.put("Facebook", "https://api.xminu.com/oauth2/authorization/facebook");
-        oauthList.add(facebookAuth);
+        // result.put("Facebook", "https://api.xminu.com/oauth2/authorization/facebook");
 
-        Map<String, String> githubAuth = new HashMap<>();
-        githubAuth.put("Github", "https://api.xminu.com/oauth2/authorization/github");
-        oauthList.add(githubAuth);
-
-        Map<String, String> discordAuth = new HashMap<>();
-        discordAuth.put("Discord", "https://api.xminu.com/oauth2/authorization/discord");
-        oauthList.add(discordAuth);
-
-        // 네이버
-
-        // 카카오
-
-        // 토스
-
-        // 라인
+        // instagram는 Oauth2를 지원하지 않음
+        // result.put("Instagram", "https://api.xminu.com/oauth2/authorization/instagram");
 
         // 애플 : 돈 내야댐 안함, X : Oauth2 너무 복잡함 안함, 마소 : 로그인이 안됨 안함
+        // 토스 : 사업자 등록증 필요함 안함
 
-        Map<String, List<Map<String, String>>> result = new HashMap<>();
-        result.put("result", oauthList);
         return ResponseEntity.ok(result);
     }
 
